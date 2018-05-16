0bb1a2ce2df65af98aff654fb3a28bf6e2b00142
Lassi <lazzikka@gmail.com>
2018-05-16 20:03:43 +0300

    finishing

--- quizanswers.php
+++ quizanswers.php
@@ -1,3 +1,21 @@
+<!-- Correct answers
+1: D
+2: C
+3: B
+4: A
+5:
+6:
+7:
+8:
+9:
+10:
+11:
+12:
+13:
+14:
+15:
+-->
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +26,7 @@
 </head>
 <body>
     <div id="page-wrap">
-        <h1 class="transparent index-headline" >Here are the results indicating your PHP knowledge</h1>
+        <h1 class="transparent index-headline" >Here are the results</h1>
         
         <?php
             $answer1 = $_POST['question-1-answers'];
@@ -17,46 +35,65 @@
             $answer4 = $_POST['question-4-answers'];
             $answer5 = $_POST['question-5-answers'];
 
-            $totalA = 0;
-            $totalB = 0;
-            $totalC = 0;
-            $totalD = 0;
+            $totalCorrectAnswers = 0;
+			$maxTotal = 4;
+			$questionsToExplain = array();
 			
-			// TODO: Only give 1 point for a correct answer, 0 for others.
+            if ($answer1 == "D") { $totalCorrectAnswers++; }
+			else (array_push($questionsToExplain, 1));
+
+            if ($answer2 == "C") { $totalCorrectAnswers++; }
+			else (array_push($questionsToExplain, 2));
 			
-            if ($answer1 == "A") { $totalA = $totalA + 1.17; $totalD = $totalD + .06; }
-            if ($answer1 == "B") { $totalB = $totalB + 1.15; $totalC = $totalC + .05; }
-            if ($answer1 == "C") { $totalC = $totalC + 1.13; $totalA = $totalA + .05; }
-            if ($answer1 == "D") { $totalD = $totalD + 1.16; $totalA = $totalA + .07; }
-
-            if ($answer2 == "A") { $totalA = $totalA + 1.23; }
-            if ($answer2 == "B") { $totalB = $totalB + 1.15; }
-            if ($answer2 == "C") { $totalC = $totalC + 1.13; }
-            if ($answer2 == "D") { $totalD = $totalD + 1.16; }
-
-            if ($answer3 == "A") { $totalA = $totalA + 1.17; $totalC = $totalC + .05; }
-            if ($answer3 == "B") { $totalB = $totalB + 1.15; $totalC = $totalC + .03; }
-            if ($answer3 == "C") { $totalC = $totalC + 1.13; $totalB = $totalB + .07; }
-            if ($answer3 == "D") { $totalD = $totalD + 1.16; }
-
-            if ($answer4 == "A") { $totalA = $totalA + 1.17; }
-            if ($answer4 == "B") { $totalB = $totalB + 1.15; }
-            if ($answer4 == "C") { $totalC = $totalC + 1.13; $totalA = $totalA + .05; $totalB = $totalB + .06; $totalD = $totalD + .07; }
-            if ($answer4 == "D") { $totalD = $totalD + 1.16; $totalB = $totalB + .04; $totalA = $totalA + .045; $totalC = $totalC + .034; }
-
-            if ($answer5 == "A") { $totalA = $totalA + 1.17; $totalD = $totalD + .08; }
-            if ($answer5 == "B") { $totalB = $totalB + 1.15; }
-            if ($answer5 == "C") { $totalC = $totalC + 1.14; $totalA = $totalA + .06; $totalD = $totalD + .08; }
-            if ($answer5 == "D") { $totalD = $totalD + 1.16; $totalC = $totalC + .04; }
+            if ($answer3 == "B") { $totalCorrectAnswers++; }
+			else (array_push($questionsToExplain, 3));
+			
+            if ($answer4 == "A") { $totalCorrectAnswers++; }
+			else (array_push($questionsToExplain, 4));
 
 			?>
 			
-			 <div class="results-overlay">
+		<div class="results-overlay">
 
             <?php
-            if ($totalA > $totalB && $totalA > $totalC && $totalA > $totalD) {
-                  
-            }
-        ?>     
-                </div>
-            </div>
+            
+			if ($totalCorrectAnswers == $maxTotal) {
+				 echo " Congratulations! ";
+			 }
+            echo $totalCorrectAnswers . " out of " . $maxTotal . " of your answers were correct. \n ";
+			
+			$questionsToExplainLength = count($questionsToExplain);
+			$whileCount = 0;
+			
+			if ($questionsToExplainLength != 0) {
+				if ($questionsToExplainLength == 1) {
+					echo "Please revise your PHP knowledge regarding question ";
+				}
+				else {
+					echo "Please revise your PHP knowledge regarding questions ";
+				}
+			}
+
+			while ($whileCount != $questionsToExplainLength) {
+				// Handling case of one wrong answer:
+				if ($questionsToExplainLength - $whileCount == 1) {
+					echo $questionsToExplain[$whileCount] . ".";
+					$whileCount++;
+				}
+				
+				// Handling case of two wrong answers:
+				elseif ($questionsToExplainLength - $whileCount == 2) {
+					echo $questionsToExplain[$whileCount] . " and " . $questionsToExplain[++$whileCount] . ".";			
+					$whileCount++;
+				}
+				
+				// Handling case of more than two wrong answers:
+				elseif ($questionsToExplainLength - $whileCount >= 3) {
+					echo $questionsToExplain[$whileCount] . ", ";
+					$whileCount++;
+				}
+			}
+			
+            ?>     
+        </div>
+    </div>
--- quizquestions.php
+++ quizquestions.php
@@ -1,9 +1,27 @@
+<!-- Correct answers
+1: D
+2: C
+3: B
+4: A
+5:
+6:
+7:
+8:
+9:
+10:
+11:
+12:
+13:
+14:
+15:
+-->
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>PHP Knowledge quiz</title>
-    <link rel="stylesheet" type="text/css" href="css/quizstyle.css" />
+    <link rel="stylesheet" type="text/css" href="quizstyle.css" />
     <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -13,89 +31,108 @@
             <ul id="test-questions">
                 <li>
                     <div class="quiz-overlay"></div>
-                    <h3>Question 1</h3>
+                    <h3>What values will the code below output? </h3>
+					<code>
+					$x = 3; <br>
+					echo $x; <br>
+					echo $x+++$x++; <br>
+					echo $x; <br>
+					echo $x---$x--; <br>
+					echo $x; <br>
+					<br>
+					</code>
                     <div class="mtm">
                         <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
-                        <label for="question-1-answers-A" class="fwrd labela">a.  Answer 1</label>
+                        <label for="question-1-answers-A" class="fwrd labela">a.  "3, 9, 5, 1, 3"</label>
                     </div>
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
-                        <label for="question-1-answers-B" class="fwrd labelb">b.  Answer 2</label>
+                        <label for="question-1-answers-B" class="fwrd labelb">b.  "3, 9, 3, 1, 3"</label>
                     </div>
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
-                        <label for="question-1-answers-C" class="fwrd labelc">c.  Answer 3</label>
+                        <label for="question-1-answers-C" class="fwrd labelc">c.  "3, 7, 3, 1, 5"</label>
                     </div>
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
-                        <label for="question-1-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
+                        <label for="question-1-answers-D" class="fwrd labeld">d.  "3, 7, 5, 1, 3"</em></label>
                     </div>
-                    <p class="quiz-progress">1 of 15</p>
+
                 </li>
                 <li>
                     <div class="quiz-overlay"></div>
-                    <h3>Question 2</h3>
+                    <h3>What is the output of the following code?</h3>
+					<code> 
+					$a = '4'; <br>
+					$b = &$a; <br>
+					$b = "2$b"; <br>
+					echo $a.", ".$b; <br>
+					<br>
+					</code>
                     <div class="mtm">
                         <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
-                        <label for="question-2-answers-A" class="fwrd labela">a.  Answer 1</label>
+                        <label for="question-2-answers-A" class="fwrd labela">a.  "4, 8"</label>
                     </div>
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
-                        <label for="question-2-answers-B" class="fwrd labelb">b.  Answer 2</label>
+                        <label for="question-2-answers-B" class="fwrd labelb">b.  "4, 4"</label>
                     </div>
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
-                        <label for="question-2-answers-C" class="fwrd labelc">c.  Answer 3</label>
+                        <label for="question-2-answers-C" class="fwrd labelc">c.  "24, 24</label>
                     </div>
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
-                        <label for="question-2-answers-D" class="fwrd labeld">d.  Answer 4</label>
+                        <label for="question-2-answers-D" class="fwrd labeld">d.  "4, 24</label>
                     </div>
-                    <p class="quiz-progress">2 of 15</p>
                 </li>
                 <li>
                     <div class="quiz-overlay"></div>
-                    <h3>Question 3</h3>
+                    <h3>What will $x be equal to after the statement $x = 2 + "50%" + "$20"?</h3>
                     <div class="mtm">
                         <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
-                        <label for="question-3-answers-A" class="fwrd labela">a.  Answer 1</label>
+                        <label for="question-3-answers-A" class="fwrd labela">a.  2</label>
                     </div>
                     <div>
                         <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
-                        <label for="question-3-answers-B" class="fwrd labelb">b.  Answer 2</label>
+                        <label for="question-3-answers-B" class="fwrd labelb">b.  52</label>
                     </div>
                     <div>
                         <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
-                        <label for="question-3-answers-C" class="fwrd labelc">c.  Answer 3</label>
+                        <label for="question-3-answers-C" class="fwrd labelc">c.  3</label>
                     </div>
                     <div>
                         <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
-                        <label for="question-3-answers-D" class="fwrd labeld">d.  Answer 4</label>
+                        <label for="question-3-answers-D" class="fwrd labeld">d.  72</label>
                     </div>
-                    <p class="quiz-progress">3 of 15</p>
                 </li>
                 <li>
                     <div class="quiz-overlay"></div>
-                    <h3>Question 4</h3>
+                    <h3>After the code below is executed, what will be the value of $text and what will strlen($text) return?</h3>
+					<code>
+					$text = 'Hihii '; <br>
+					$text[10] = 'Hohoo';
+					<br>
+					</code>
                     <div class="mtm">
                         <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
-                        <label for="question-4-answers-A" class="fwrd labela">a.  Answer 1</label>
+                        <label for="question-4-answers-A" class="fwrd labela">a.  "Hihii &nbsp&nbsp&nbsp&nbspH" </label>
                     </div>
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
-                        <label for="question-4-answers-B" class="fwrd labelb">b.  Answer 2</label>
+                        <label for="question-4-answers-B" class="fwrd labelb">b.  An out of bounds -error</label>
                     </div>
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
-                        <label for="question-4-answers-C" class="fwrd labelc">c.  Answer 3</label>
+                        <label for="question-4-answers-C" class="fwrd labelc">c.  "Hihii"</label>
                     </div>
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
-                        <label for="question-4-answers-D" class="fwrd labeld">d.  Answer 4</label>
+                        <label for="question-4-answers-D" class="fwrd labeld">d.  "Hihii &nbsp&nbsp&nbsp&nbspHohoo"</label>
                     </div>
-                    <p class="quiz-progress">4 of 15</p>
                 </li>
-                <li>
+                <!--
+				<li>
                     <div class="quiz-overlay"></div>
                     <h3>Question 5</h3>
                     <div class="mtm">
@@ -114,7 +151,6 @@
                         <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
                         <label for="question-5-answers-D" class="fwrd labeld">d.  Answer 4</label>
                     </div>
-                    <p class="quiz-progress">5 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -135,7 +171,6 @@
                         <input type="radio" name="question-6-answers" id="question-6-answers-D" value="D" />
                         <label for="question-6-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">6 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -156,7 +191,6 @@
                         <input type="radio" name="question-7-answers" id="question-7-answers-D" value="D" />
                         <label for="question-7-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">7 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -177,7 +211,6 @@
                         <input type="radio" name="question-8-answers" id="question-8-answers-D" value="D" />
                         <label for="question-8-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">8 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -198,7 +231,6 @@
                         <input type="radio" name="question-9-answers" id="question-9-answers-D" value="D" />
                         <label for="question-9-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">9 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -219,7 +251,6 @@
                         <input type="radio" name="question-10-answers" id="question-10-answers-D" value="D" />
                         <label for="question-10-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">10 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -240,7 +271,6 @@
                         <input type="radio" name="question-11-answers" id="question-11-answers-D" value="D" />
                         <label for="question-11-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">11 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -261,7 +291,6 @@
                         <input type="radio" name="question-12-answers" id="question-12-answers-D" value="D" />
                         <label for="question-12-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">12 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -282,7 +311,6 @@
                         <input type="radio" name="question-13-answers" id="question-13-answers-D" value="D" />
                         <label for="question-13-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">13 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -303,7 +331,6 @@
                         <input type="radio" name="question-14-answers" id="question-14-answers-D" value="D" />
                         <label for="question-14-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">14 of 15</p>
                 </li>
 				<li>
                     <div class="quiz-overlay"></div>
@@ -324,18 +351,17 @@
                         <input type="radio" name="question-15-answers" id="question-15-answers-D" value="D" />
                         <label for="question-15-answers-D" class="fwrd labeld">d.  Answer 4</em></label>
                     </div>
-                    <p class="quiz-progress">15 of 15</p>
                 </li>
+				-->
                 <li>
                     <div class="quiz-overlay"></div>
-                    <h3 class="anticipate">The results follow</h3>
+                    <h3 class="anticipate">The results will follow soon..</h3>
                     <input type="submit" value="Submit Quiz" id="submit-quiz" />
                 </li>
             </ul>
 	</form>
         <div class="nomargin"></div>
     </div>
-    <!-- Does not work on JYU servers?
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
 	<script>
            (function($) {
@@ -350,6 +376,5 @@
               });
            }(jQuery))
     </script>
-	-->
 </body>
 </html>
